d9dddcbaf4a2cc7f539aab82b659daadc7c02aed
jvcss <jvcs.mecatronica@gmail.com>
2024-05-10 05:24:00 -0300

    mais uma receita

--- lib/receitas.dart
+++ lib/receitas.dart
@@ -7,6 +7,7 @@ class Receita {
 }
 
 List<Receita> receitas = [
+  
   Receita(
     "Smoothie de Banana e Espinafre",
     ["Banana", "Folhas de espinafre", "Água"],
@@ -22,5 +23,10 @@ List<Receita> receitas = [
     ["Filé de salmão", "Cúrcuma em pó", "Sal e pimenta a gosto", "Azeite de oliva"],
     "1. Tempere o filé de salmão com sal, pimenta e cúrcuma em pó. \n2. Aqueça uma frigideira com azeite de oliva. \n3. Grelhe o salmão por alguns minutos de cada lado, até ficar dourado e cozido por dentro."
   ),
+
+  Receita(
+    'Salada de folhas verdes', 
+    ['folhas verdes escuras (couve, espinafre, agrião)', 'banana', 'abacate', 'amêndoas', 'castanha do Pará', 'grão de bico', 'lentilha', 'feijão preto'], 
+    'Misture as folhas verdes com banana, abacate, amêndoas, castanha do Pará, grão de bico, lentilha e feijão preto. Tempere a gosto e sirva como acompanhamento.')
   // Adicione mais receitas conforme desejar
 ];
